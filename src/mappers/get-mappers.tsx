f3091e6a7f4f4be32ab107d96d19d4deb687b1ba
Dagron <dagron.pokeabyte.io@gmail.com>
2024-09-05 10:13:48 -0700

    Fixed TS error

--- src/mappers/get-mappers.tsx
+++ src/mappers/get-mappers.tsx
@@ -1,21 +1,24 @@
 ﻿import config from "../../config.json"
 import  {useEffect, useState} from "react";
 
+interface Mapper {
+    id: string;
+    displayName: string;
+}
 
-const fetchData = async () => {
+const fetchData = async () : Promise<Mapper[]> => {
     const url = config.pokeabyte_url + "/mapper-service/get-mappers"
     const resp = await fetch(url, {
         method: "GET"
     });
-    const data = await resp.json();
-    return data;
+    return await resp.json();
 };
 
 export default function GetMappers() {
-    const [mapperData, getMapperData] = useState(null);
+    const [mapperData, setMapperData] = useState<Mapper[] | null> (null);
     useEffect(() => {
         fetchData().then((data) => {
-            getMapperData(data);
+            setMapperData(data);
         });
     }, [])
     return (
@@ -23,7 +26,7 @@ export default function GetMappers() {
             <h1>Mappers</h1>
             { mapperData ? (
                 <ul>
-                    {mapperData.map(({id, displayName} : any) => {
+                    {mapperData.map(({id, displayName}) => {
                         return <li key={id}>{displayName}</li>;
                     })}
                 </ul>
