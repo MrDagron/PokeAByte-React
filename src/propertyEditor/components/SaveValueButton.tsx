c8e0daf0c6c3e1327bfb51e16a99b28bbae467e3
StringEpsilon <StringEpsilon@gmail.com>
2024-09-05 19:29:25 +0200

    Fix: Properly assign the onClick callback of SaveValueButton.

--- src/propertyEditor/components/SaveValueButton.tsx
+++ src/propertyEditor/components/SaveValueButton.tsx
@@ -1,7 +1,7 @@
 export function SaveValueButton({ active, onClick }: { value: string, active: boolean, onClick: Function }) {
   const classes = active ? "material-icons" : "material-icons inactive";
   return (
-    <button type="button" onClick={onClick()}>
+    <button type="button" onClick={() => onClick()}>
       <span className={classes}>
         save
       </span>
