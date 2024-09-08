34b5a4538f7610c698cce2fcf9174fef17a252a3
StringEpsilon <StringEpsilon@gmail.com>
2024-09-08 21:44:22 +0200

    Base layout work.
    - Added proper header
    - Added main
    - Added some basic headlines
    - Misc. css

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,47 @@
 
-import React from 'react';
+import React, { useSyncExternalStore } from 'react';
 import './App.css'
 import GetMappers from "./mappers/get-mappers.tsx";
 import { PropertyEditor } from './propertyEditor/PropertyEditor.tsx';
 import { ToastContainer } from './notifications/ToastContainer.tsx';
+import { Store } from './propertyEditor/store/propertyStore.ts';
 
 function App() {
   // todo: Use a proper router or something.
-	const [viewState, setViewState] = React.useState<"mappers" | "properties">("properties");
+  const mapper = useSyncExternalStore(Store.subscribeMapper, Store.getMapper);
+  const [viewState, setViewState] = React.useState<"mappers" | "properties">("properties");
+  const highlightClass = mapper ? "connected" : "disconnected";
+  return (
+    <>
+      <header className={highlightClass}>
+        <h1 >
+          Poke-A-Byte
+        </h1>
+        <div>
+          <button type="button" onClick={() => setViewState("mappers")} >
+            <span className="material-icons"> catching_pokemon </span> Mappers
+          </button>
+          <button type="button" onClick={() => setViewState("properties")} >
+            <span className="material-icons"> api </span> 
+            Properties
+          </button>
+        </div>
+        <button type="button">
+          <span className={`material-icons ${highlightClass}`}>
+            power_settings_new
+          </span>
+        </button>
+      </header>
+      <main>
+        {viewState === "mappers"
+          ? <GetMappers />
+          : <PropertyEditor />
+        }
+      </main>
+      <ToastContainer />
+    </>
+  )
 
-		return (
-			<>
-        <header>
-          <div>
-            <button type="button" onClick={() => setViewState("mappers")} >
-              Mappers
-            </button>
-            <button type="button" onClick={() => setViewState("properties")} >
-              Properties
-            </button>
-          </div>
-        </header>
-				{viewState === "mappers" 
-					? <GetMappers />
-					: <PropertyEditor />
-				}
-        <ToastContainer />
-			</>
-		)
-	
 }
 
 export default App
--- src/mappers/get-mappers.tsx
+++ src/mappers/get-mappers.tsx
@@ -1,38 +1,44 @@
 ﻿import config from "../../config.json"
-import  {useEffect, useState} from "react";
+import { useEffect, useState, useSyncExternalStore } from "react";
+import { Store } from "../propertyEditor/store/propertyStore";
 
 interface Mapper {
-    id: string;
-    displayName: string;
+  id: string;
+  displayName: string;
 }
 
-const fetchData = async () : Promise<Mapper[]> => {
-    const url = config.pokeabyte_url + "/mapper-service/get-mappers"
-    const resp = await fetch(url, {
-        method: "GET"
-    });
-    return await resp.json();
+const fetchData = async (): Promise<Mapper[]> => {
+  const url = config.pokeabyte_url + "/mapper-service/get-mappers"
+  const resp = await fetch(url, {
+    method: "GET"
+  });
+  return await resp.json();
 };
 
 export default function GetMappers() {
-    const [mapperData, setMapperData] = useState<Mapper[] | null> (null);
-    useEffect(() => {
-        fetchData().then((data) => {
-            setMapperData(data);
-        });
-    }, [])
-    return (
-        <>
-            <h1>Mappers</h1>
-            { mapperData ? (
-                <ul>
-                    {mapperData.map(({id, displayName}) => {
-                        return <li key={id}>{displayName}</li>;
-                    })}
-                </ul>
-            ) : (
-                <p>Loading...</p>
-            )}
-        </>
-    );
+  const mapper = useSyncExternalStore(Store.subscribeMapper, Store.getMapper);
+  const [mapperData, setMapperData] = useState<Mapper[] | null>(null);
+  useEffect(() => {
+    fetchData().then((data) => {
+      setMapperData(data);
+    });
+  }, [])
+  return (
+    <>
+      <h2>Mapper Manager</h2>
+      { mapper
+        ? <p>Mapper Loaded: {mapper.gameName} </p>
+        : <p>No Mapper Loaded </p>
+      }
+      {mapperData ? (
+        <ul>
+          {mapperData.map(({ id, displayName }) => {
+            return <li key={id}>{displayName}</li>;
+          })}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
+  );
 }
